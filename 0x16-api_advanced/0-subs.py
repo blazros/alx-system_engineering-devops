7bfec643ba301b477a78fe739dbcee9caec2920f
Ahmed Osama Abd El-Samie <110006931+blazros@users.noreply.github.com>
2024-08-28 03:10:53 +0300

    Update 0-subs.py

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -8,7 +8,7 @@ def number_of_subscribers(subreddit):
     url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
     
     response = requests.get(url, allow_redirects=False)
-    if response.status_code == 404:
+    if response.error == 404:
         return 0
     results = response.json().get("data")
     return results.get("subscribers")
