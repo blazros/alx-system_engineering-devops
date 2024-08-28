c69bc6e9828d83e0596a4c61b757bcc61fa41423
Ahmed Osama Abd El-Samie <110006931+blazros@users.noreply.github.com>
2024-08-28 03:31:21 +0300

    Update 0-subs.py

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -4,22 +4,15 @@ import requests
 
 def number_of_subscribers(subreddit):
     """Return the total number of subscribers on a given subreddit."""
-    # Set a custom User-Agent to avoid being blocked by Reddit
-    headers = {'User-Agent': 'Mozilla/5.0'}
-    
-    # Make a GET request to the Reddit API
-    url = f'https://www.reddit.com/r/{subreddit}/about.json'
-    
-    try:
-        response = requests.get(url, headers=headers, allow_redirects=False)
-        
-        # Check if the response is successful
-        if response.status_code == 200:
-            data = response.json()
-            return data['data']['subscribers']
-        else:
-            # If the subreddit is invalid or not found, return 0
-            return 0
-    except requests.exceptions.RequestException:
-        # Handle any request exceptions
+    url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
+    headers = {
+        "User-Agent": "Zephyronne"
+    }
+    response = requests.get(url, headers=headers, allow_redirects=False)
+    if response.status_code == 200:
+        data = response.json().get("data")
+        return data.get("subscribers")
+    elif requests.exceptions.RequestException:
+        return 0
+    else:
         return 0
