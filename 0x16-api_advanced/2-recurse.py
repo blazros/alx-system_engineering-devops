92bb849de993c0a6582d63c48643db649b282550
blazros <blazros78@gmail.com>
2024-08-29 22:39:41 +0300

    2 Pycodestyle

--- 0x16-api_advanced/2-recurse.py
+++ 0x16-api_advanced/2-recurse.py
@@ -5,21 +5,20 @@ import requests
 
 
 def recurse(subreddit, hot_list=[], after=None):
-    """Recursively returns a list of titles of all hot posts for a given subreddit."""
+    """Recursively returns a list of titles of all hot posts
+    for a given subreddit."""
     url = f'https://www.reddit.com/r/{subreddit}/hot.json'
     headers = {'User-Agent': 'Mozilla/5.0'}
     params = {'after': after, 'limit': 100}
-    
     try:
-        response = requests.get(url, headers=headers, params=params, allow_redirects=False)
+        response = requests.\
+            get(url, headers=headers, params=params, allow_redirects=False)
         if response.status_code != 200:
             return None
-        
         data = response.json().get('data', {})
         children = data.get('children', [])
         for post in children:
             hot_list.append(post.get('data', {}).get('title'))
-        
         after = data.get('after')
         if after is not None:
             return recurse(subreddit, hot_list, after)
